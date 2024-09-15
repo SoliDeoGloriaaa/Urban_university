b2213e3ff27c4a440cff6dabe350bee5b3bb3213
Александр Афанасьев <knowthepain00@mail.ru>
2024-09-15 16:49:56 +0300

    dz_5

---
+++ module_10/dz_5.py
@@ -0,0 +1,30 @@
+import multiprocessing
+import datetime
+
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as file:
+        line = file.readline()
+        while line:
+            all_data.append(line)
+            line = file.readline()
+    return all_data
+
+
+file_list = ['file_1.txt', 'file_2.txt', 'file_3.txt', 'file_4.txt']
+
+now = datetime.datetime.now()
+for file_name in file_list:
+    read_info(file_name)
+end = datetime.datetime.now()
+print(f"Линейный подход занял {end - now} секунд")
+# Линейный подход занял 0:00:08.369612 секунд
+
+if __name__ == '__main__':
+    with multiprocessing.Pool(processes=4) as pool:
+        now = datetime.datetime.now()
+        results = pool.map(read_info, file_list)
+    end = datetime.datetime.now()
+    print(f"Многопроцессный подход занял {end - now} секунд")
+# Многопроцессный подход занял 0:00:19.522156 секунд
