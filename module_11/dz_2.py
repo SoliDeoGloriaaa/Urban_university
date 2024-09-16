9752d477f6e78dece470ea06b365863a54e3eca0
Александр Афанасьев <knowthepain00@mail.ru>
2024-09-16 18:10:17 +0300

    ПРАВКИ

--- module_11/dz_2.py
+++ module_11/dz_2.py
@@ -4,7 +4,7 @@ import pprint
 def introspection_info(obj):
     obj_type = type(obj).__name__
     attributes = [attr for attr in dir(obj) if not attr.startswith('__')]
-    methods = [method for method in dir(obj) if callable(getattr(obj, method))]
+    methods = [method for method in dir(obj) if callable(getattr(obj, method)) and not method.startswith('__')]
     obj_module = getattr(obj, '__module__', 'builtins' if obj_type in dir(__builtins__) else None)
     result = {
         'type': obj_type,
