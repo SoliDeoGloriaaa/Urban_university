6a837213cc19e20f60b9b3fdb1f4ccacfb433d59
Александр Афанасьев <knowthepain00@mail.ru>
2024-10-29 19:26:13 +0300

    dz_5_md_14

--- module_14/crud_functions.py
+++ module_14/crud_functions.py
@@ -2,6 +2,7 @@ import sqlite3
 
 DB_NAME = 'products.db'
 
+
 def initiate_db():
     connection = sqlite3.connect(DB_NAME)
     cursor = connection.cursor()
@@ -12,6 +13,16 @@ def initiate_db():
             title TEXT NOT NULL,
             description TEXT,
             price INTEGER NOT NULL
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL,
+            age INTEGER NOT NULL,
+            balance INTEGER NOT NULL DEFAULT 1000
         )
     ''')
 
@@ -40,6 +51,31 @@ def populate_db():
     cursor.executemany('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)', products)
     conn.commit()
     conn.close()
+
+
+def add_user(username, email, age):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)
+    ''', (username, email, age, 1000))
+
+    conn.commit()
+    conn.close()
+
+
+def is_included(username):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT COUNT(*) FROM Users WHERE username = ?', (username,))
+    count = cursor.fetchone()[0]
+
+    conn.close()
+
+    return count > 0
+
 
 if __name__ == '__main__':
     initiate_db()
