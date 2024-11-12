4b99190027a0bb33e9058be9d62902511fdfdff5
Александр Афанасьев <knowthepain00@mail.ru>
2024-11-13 00:05:06 +0300

    .

---
+++ first_dz.py
@@ -0,0 +1,21 @@
+# 1st program
+result = 9 ** 0.5 * 5
+print(result)
+
+# 2nd program
+result = (9.99 > 9.98) and (1000 != 1000.1)
+print(result)
+
+# 3rd program
+result1 = 2 * 2 + 2
+result2 = 2 * (2 + 2)
+print(result1)
+print(result2)
+print(result1 == result2)
+
+# 4th program
+number_str = '123.456'
+number_float = float(number_str)
+number_float *= 10
+first_digit = int(number_float) % 10
+print(first_digit)
